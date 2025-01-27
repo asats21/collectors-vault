3061e1e2b43789beae810ec0ea2297f368f82312
asats21 <satsalpha@proton.me>
2025-01-27 21:36:05 +0300

    commit

--- src/SatsTable.js
+++ src/SatsTable.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactPaginate from 'react-paginate';
-import { FaTrash } from 'react-icons/fa';
+import { FaTrash, FaGem, FaPizzaSlice, FaFont, FaHive, FaDiceTwo, FaDiceThree } from 'react-icons/fa';
+import { FaBluesky } from "react-icons/fa6";
 
 const SatsTable = ({ currentSats, offset, handleDelete, pageCount, handlePageClick }) => {
+
+  // Mapping tags to corresponding Font Awesome icons and colors
+  const tagIcons = {
+    alpha: <FaFont className="icon" />,
+    uncommon: <FaGem className="icon" />,
+    black_uncommon: <FaGem className="icon" />,
+    pizza: <FaPizzaSlice className="icon" />,
+    palindrome: <FaBluesky className="icon" />,
+    uniform: <FaBluesky className="icon" />,
+    perfect: <FaBluesky className="icon" />,
+    paliblock: <FaHive className="icon" />,
+    '2_digits': <FaDiceTwo className="icon" />,
+    '3_digits': <FaDiceThree className="icon" />,
+  };
+
   return (
     <>
       <div className="table-responsive">
@@ -25,11 +41,27 @@ const SatsTable = ({ currentSats, offset, handleDelete, pageCount, handlePageCli
                 <td>#{sat}</td>
                 <td>
                   <div className="sat-tags">
-                    {details.tags.map((tag) => (
-                      <span key={tag} className={`tag-${tag.replace(' ', '-')}`}>
-                        {tag}
-                      </span>
-                    ))}
+                  {details.tags
+                      .map((tag) => {
+                        const icon = tagIcons[tag];
+                        if (icon) {
+                          return { type: 'icon', icon }; // Return the icon if it exists
+                        }
+                        return { type: 'tag', tag }; // Return the tag text if no icon
+                      })
+                      .sort((a, b) => {
+                        // Sort icons first, then tags
+                        return a.type === 'icon' ? -1 : 1;
+                      })
+                      .map((item, index) =>
+                        item.type === 'icon' ? (
+                          React.cloneElement(item.icon, { key: index })
+                        ) : (
+                          <span key={item.tag} className={`tag-${item.tag.replace(' ', '-')}`}>
+                            {item.tag}
+                          </span>
+                        )
+                  )}
                   </div>
                 </td>
                 <td>{details.block_number}</td>
