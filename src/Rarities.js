ff24da260653b5684a567378d8e876fc5b33ce93
asats21 <satsalpha@proton.me>
2025-02-06 20:50:44 +0300

    commit

--- src/Rarities.js
+++ src/Rarities.js
@@ -14,6 +14,96 @@ export const rarities = [
         "found": "21K",
     },
     {
+        "tags": ["2009", "alpha"],
+        "total": "1,6M",
+        "found": "8K",
+    },
+    {
+        "tags": ["3_digits", "palindrome"],
+        "total": "1,2M",
+        "found": "24K",
+    },
+    {
+        "tags": ["black_uncommon", "epoch0"],
+        "total": "210K",
+        "found": "13K",
+    },
+    {
+        "tags": ["3_digits", "paliblock"],
+        "total": "126,465",
+    },
+    {
+        "tags": ["3_digits", "uniform_palinception"],
+        "total": "139,616",
+    },
+    {
+        "tags": ["vintage", "palindrome", "paliblock"],
+        "total": "1,1M",
+        "found": "2K",
+    },
+    {
+        "tags": ["rodarmor_name"],
+        "total": "12,450",
+        "found": "1K",
+    },
+    {
+        "tags": ["vintage", "palindrome", "3_digits"],
+        "total": "244,800",
+    },
+    {
+        "tags": ["uncommon", "2009"],
+        "total": "32,474",
+    },
+    {
+        "tags": ["jpeg", "palindrome"],
+        "total": "5,000",
+    },
+    {
+        "tags": ["alpha", "pizza"],
+        "total": "10,002",
+    },
+    {
+        "tags": ["3_digits", "uniform_palinception", "paliblock"],
+        "total": "25,173",
+    },
+    {
+        "tags": ["palindrome", "pizza", "pizza_4/20"],
+        "total": "1,600",
+    },
+    {
+        "tags": ["alpha", "palindrome", "2009"],
+        "total": "3,000",
+    },
+    {
+        "tags": ["perfect_palinception", "2_digits"],
+        "total": "1,768",
+    },
+    {
+        "tags": ["alpha", "pizza", "2009"],
+        "total": "300",
+    },
+    {
+        "tags": ["uniform_palinception", "nova"],
+        "total": "2,040",
+        "found": "135",
+    },
+    {
+        "tags": ["jpeg", "palindrome", "jpeg_2010"],
+        "total": "5,000",
+    },
+    {
+        "tags": ["alpha", "jpeg"],
+        "total": "500",
+    },
+    {
+        "tags": ["paliblock", "uncommon", "alpha"],
+        "total": "1,602",
+    },
+    {
+        "tags": ["paliblock", "black_uncommon", "omega"],
+        "total": "1,602",
+    },
+    {
         "tags": ["uncommon"],
         "total": "6,9M",
         "found": "170K",
@@ -39,13 +129,26 @@ export const rarities = [
         "found": "46K",
     },
     {
+        "tags": ["perfect_palinception"],
+        "total": "13,305",
+        "found": 335,
+    },
+    {
+        "tags": ["perfect_palinception", "nova"],
+        "total": "1,050",
+    },
+    {
         "tags": ["palindrome", "uniform_palinception", "perfect_palinception"],
         "total": 13305,
         "found": 335,
     },
     {
+        "tags": ["palindrome", "uncommon"],
+        "total": "7,925",
+    },
+    {
         "tags": ["palindrome", "uniform_palinception", "perfect_palinception", "nova"],
-        "total": "1K",
+        "total": "1,050",
     }
 ];
 
