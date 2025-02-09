2a8616f6a4699fbe3b0ed6bbfe39fd736b347e6e
asats21 <satsalpha@proton.me>
2025-02-09 17:07:10 +0300

    commit

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { tagWeights, getWeightStats } from './tagWeights';
 import { LeaderboardWallets } from './LeaderboardWallets';
 import { RenderTags } from "./RenderTags";
+import { getSubPaliLength, displayUniformPalinception } from "./TagDetection";
+import { getRodarmorName, isRodarmorName } from './RodarmorNames.js';
 import { FaCrown } from "react-icons/fa6";
 import { MdOutlineNumbers } from "react-icons/md";
 import { BiSolidPurchaseTag } from "react-icons/bi";
@@ -19,25 +21,37 @@ const Leaderboard = () => {
   // Sort wallets by total weight (descending)
   const sortedWalletData = walletData.sort((a, b) => b.totalWeight - a.totalWeight);
 
+  const displaySatNumber = (sat) => {
+    const subPaliLength = getSubPaliLength(sat);
+    if (subPaliLength) {
+      return '#' + displayUniformPalinception(sat, subPaliLength);
+    }
+    if(isRodarmorName(sat)) {
+      return getRodarmorName(sat);
+    }
+
+    return '#' + sat;
+  }
+
   return (
     <div className="container">
       {/* Header */}
       <div className="page-header mt-4 mt-md-2" style={{"margin-bottom": "0px"}}>
         <h1>Demo Leaderboard</h1>
       </div>
-      <p>See where your collection stands in our demo wallets list</p>
+      <p>Discover how your collection ranks among our demo wallets</p>
 
       {/* Leaderboard Table */}
       <div className="table-responsive">
-        <table className="table table-dark table-striped table-hover">
+        <table className="table table-dark table-striped">
           <thead>
             <tr>
               <th scope="col">#</th>
               <th scope="col">Wallet</th>
-              <th scope="col">Total Weight</th>
-              <th scope="col">Number of Items</th>
-              <th scope="col"><FaCrown className="icon" style={{ color: "#F2A900" }} /><MdOutlineNumbers className="icon" style={{ color: "#F2A900" }} /></th>
-              <th scope="col"><FaCrown className="icon" style={{ color: "#F2A900" }} /><BiSolidPurchaseTag  className="icon" style={{ color: "#F2A900" }} /></th>
+              <th scope="col">Score</th>
+              <th scope="col"><FaCrown className="icon" style={{ color: "#F2A900" }} /> <MdOutlineNumbers className="icon" style={{ color: "#F2A900" }} /></th>
+              <th scope="col"><FaCrown className="icon" style={{ color: "#F2A900" }} /> <BiSolidPurchaseTag  className="icon" style={{ color: "#F2A900" }} /></th>
+              <th scope="col">Items</th>
             </tr>
           </thead>
           <tbody>
@@ -46,9 +60,9 @@ const Leaderboard = () => {
                 <th scope="row">{index + 1}</th>
                 <td>{wallet}</td>
                 <td>{totalWeight}</td>
-                <td>{numberOfItems}</td>
-                <td>{heaviestSat.sat}</td>
+                <td>{displaySatNumber(heaviestSat.sat)}</td>
                 <td><div className="sat-tags"><RenderTags tags={heaviestSat.details.tags} /></div></td>
+                <td>{numberOfItems}</td>
               </tr>
             ))}
           </tbody>
