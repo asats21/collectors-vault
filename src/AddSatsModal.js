33b86b134e68ba34cd06c26823a822cb444f7f24
asats21 <satsalpha@proton.me>
2025-02-15 19:39:22 +0300

    commit

--- src/AddSatsModal.js
+++ src/AddSatsModal.js
@@ -27,7 +27,7 @@ const AddSatsModal = ({ showModal, setShowModal, input, setInput, handleSubmit }
             <Form.Label className='mb-2'>
               Our app can accurately identify sat numbers even when mixed with irrelevant data. 
               Try copying and pasting multiple sats at once from the wallet checker 
-              at <a href="https://sating.io/" target="_blank" rel="noopener noreferrer" style={{ color: "#C38BFA", textDecoration: 'none', "font-weight": "bold" }}>sating.io</a> to see how well it works!
+              at <a href="https://sating.io/" target="_blank" rel="noopener noreferrer" style={{ color: "#C38BFA", textDecoration: 'none', "fontWeight": "bold" }}>sating.io</a> to see how well it works!
             </Form.Label>
 
             <Form.Label>Enter sat numbers (comma OR space OR newline separated):</Form.Label>
